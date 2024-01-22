6dd6122e6d34bc98a1cdd3e75aa218217e77288b
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-01-22 00:47:06 -0500

    set CUDA_HOME

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,9 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
     export HOROVOD_GPU_OPERATIONS=NCCL
     export HOROVOD_NCCL_LINK=SHARED
     export HOROVOD_CUDA_HOME=/usr/local/cuda
-    export CMAKE_ARGS="${CMAKE_ARGS} -D CMAKE_CUDA_ARCHITECTURES=all"
+fi
+if [[ ${cuda_compiler_version} == "12.0" ]]; then
+    export HOROVOD_CUDA_HOME=$BUILD_PREFIX
 fi
 export HOROVOD_WITH_TENSORFLOW=1
 export HOROVOD_WITH_PYTORCH=1
